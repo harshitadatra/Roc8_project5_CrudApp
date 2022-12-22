328a14b5fa737cfb4294b16d5230978eb7532da6
harshitadatra <57798468+harshitadatra@users.noreply.github.com>
2022-12-23 01:49:04 +0530

    feat: routing added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import {BrowserRouter ,Route,Routes} from "react-router-dom"
 
-import { Home } from './components/pages/Home';
-import "../node_modules/bootstrap/dist/css/bootstrap.css"
+import { Create } from './components/Create';
+
+import { Read } from './components/Read';
+import { Update } from "./components/Update";
 
 function App() {
   return (
-    <div>
-      <Home/>
+    <div className="container">
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Create/>}></Route>
+          <Route path="/read" element={<Read/>}></Route>
+          <Route path="/update" element={<Update/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
